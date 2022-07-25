71d9813bb8d128d770419b7fcdc4a01f25611be3
jordanfoote <jordan.foote@outlook.com>
2022-07-25 15:52:41 -0500

    adding home page picture

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import LogoS from '../../../asets/images/logo_sub.jpg'
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img className="solid-logo"></img>
+    </div>
+  )
+}
+
+export default Logo
