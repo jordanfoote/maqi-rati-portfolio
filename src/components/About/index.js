1ee2d4bae88075ccdc54ec77a018d9b6f0a7af8c
jordanfoote <jordan.foote@outlook.com>
2022-07-26 18:37:48 -0500

    creating about me content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -12,22 +12,30 @@ const About = () => {
           />
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          Ever since he was a young lad,{' '}
+          <strong className="highlight">MAQI RATI</strong> has had a creative
+          soul. In a world of comic books, video games, and film, MAQI gained
+          inspiration to be what he was born to be... an{' '}
+          <strong className="highlight">Artist</strong>. From custom paintings
+          and movie props to graphic design, MAQI does it all.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum
+          MAQI's creative endeavors developed into more than just visual art.
+          Discovering his passion for music has lead to many experiences in
+          being a part of the underground punk rock and metal scenes in central
+          Oklahoma. As a talented{' '}
+          <strong className="highlight">Musician</strong>, MAQI delivers his
+          unique sound through a countless variety of instruments.
+        </p>
+        <p className="quote">
+          <em>"Give me a f*cking tuba and I'll get you something out of it"</em>
+        </p>
+        <p>
+          DIY is the way. MAQI is always on the hunt for new projects to be
+          involved in. Whether you need an{' '}
+          <strong className="highlight">Actor</strong>, or a creative mind to
+          gear you towards success, he is the one you need. MAQI RATI is... the{' '}
+          <strong className="highlight">Creator</strong>.
         </p>
       </div>
     </div>
